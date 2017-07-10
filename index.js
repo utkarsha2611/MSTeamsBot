fc58ae2c01715298f704e292cc292cbdc5da3a0e
utkarsha.singh <t-utsing@microsoft.com>
2017-07-10 12:21:01 +0800

    added

--- index.js
+++ index.js
@@ -191,7 +191,8 @@ bot.dialog("/signin", [].concat(
     ba.authenticate("aadv2"),
     (session, args, skip) => {
         let user = ba.profile(session, "aadv2");
-        session.endDialog(user.displayName);
+        session.send('Hello ' + user.displayName + ', welcome to the organization');
+        session.endDialog();
         session.userData.accessToken = user.accessToken;
         session.userData.refreshToken = user.refreshToken;
         session.beginDialog('workPrompt');
