afe57a34b4a5b349087c4b859339c4bb5c8f709f
utkarsha.singh <t-utsing@microsoft.com>
2017-07-10 14:20:36 +0800

    Added .gitignore

--- index.js
+++ index.js
@@ -191,10 +191,11 @@ bot.dialog("/signin", [].concat(
     ba.authenticate("aadv2"),
     (session, args, skip) => {
         let user = ba.profile(session, "aadv2");
-        session.endDialog('Hello ' + user.displayName + ', welcome to the organization');
+        session.send('Hello ' + user.displayName + ', welcome to the organization');
         session.userData.accessToken = user.accessToken;
         session.userData.refreshToken = user.refreshToken;
-        session.beginDialog('workPrompt');
+        //session.beginDialog('workPrompt');
+        session.endDialog('Type HELP to see what I can do.');
     }
 ));
 
